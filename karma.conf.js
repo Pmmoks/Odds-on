aa977a93e2a8306bf1fdb5431859995c578008ef
Pmmoks <Pmmoks@hotmail.com>
2016-10-18 22:29:55 +0100

    configured testing framework

--- karma.conf.js
+++ karma.conf.js
@@ -5,17 +5,17 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './test/',
+    basePath: '',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha','chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'lib/*.js',
-      'test/*.js'
+      'src/*.js',
+      'test/*.spec.js'
     ],
 
     // list of files to exclude
